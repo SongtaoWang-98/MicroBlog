41cb32fc64180654fd22ea6873a193da28608584
SongtaoWang <songtao_wang@qq.com>
2022-07-20 23:03:27 +0800

    更新数据库

--- microblog.sql
+++ microblog.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.23, for macos10.15 (x86_64)
 --
 -- Host: 127.0.0.1    Database: microblog
 -- ------------------------------------------------------
--- Server version	8.0.29
+-- Server version	8.0.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -134,6 +134,32 @@ INSERT INTO `comment` VALUES (1,1,2,'确实','2022-07-18 17:01:53',0,NULL,0);
 UNLOCK TABLES;
 
 --
+-- Table structure for table `concern_group`
+--
+
+DROP TABLE IF EXISTS `concern_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `concern_group` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `name` varchar(255) DEFAULT '默认分组',
+  `deleted` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `concern_group`
+--
+
+LOCK TABLES `concern_group` WRITE;
+/*!40000 ALTER TABLE `concern_group` DISABLE KEYS */;
+INSERT INTO `concern_group` VALUES (1,1,'默认分组',0),(2,2,'默认分组',0),(3,3,'默认分组',0),(4,1,'朋友',0),(5,1,'同事',0),(6,4,'默认分组',0);
+/*!40000 ALTER TABLE `concern_group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `follow`
 --
 
@@ -157,32 +183,6 @@ LOCK TABLES `follow` WRITE;
 /*!40000 ALTER TABLE `follow` DISABLE KEYS */;
 INSERT INTO `follow` VALUES (1,1,2,0),(2,2,1,0),(3,3,1,0),(4,1,3,0),(5,4,1,0);
 /*!40000 ALTER TABLE `follow` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `group`
---
-
-DROP TABLE IF EXISTS `group`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `group` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `name` varchar(255) DEFAULT '默认分组',
-  `deleted` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `group`
---
-
-LOCK TABLES `group` WRITE;
-/*!40000 ALTER TABLE `group` DISABLE KEYS */;
-INSERT INTO `group` VALUES (1,1,'默认分组',0),(2,2,'默认分组',0),(3,3,'默认分组',0),(4,1,'朋友',0),(5,1,'同事',0),(6,4,'默认分组',0);
-/*!40000 ALTER TABLE `group` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -330,10 +330,9 @@ DROP TABLE IF EXISTS `photograph`;
 CREATE TABLE `photograph` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `photo_id` int DEFAULT NULL,
   `deleted` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -342,6 +341,7 @@ CREATE TABLE `photograph` (
 
 LOCK TABLES `photograph` WRITE;
 /*!40000 ALTER TABLE `photograph` DISABLE KEYS */;
+INSERT INTO `photograph` VALUES (1,'苏格兰',0);
 /*!40000 ALTER TABLE `photograph` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -354,10 +354,11 @@ DROP TABLE IF EXISTS `picture`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `picture` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `photograph_id` int DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   `deleted` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -366,7 +367,7 @@ CREATE TABLE `picture` (
 
 LOCK TABLES `picture` WRITE;
 /*!40000 ALTER TABLE `picture` DISABLE KEYS */;
-INSERT INTO `picture` VALUES (1,'null',0);
+INSERT INTO `picture` VALUES (1,1,'../../static/Stewart/train.jpg',0),(2,1,'../../static/Stewart/hills.jpg',0);
 /*!40000 ALTER TABLE `picture` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -462,4 +463,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-18 18:07:43
+-- Dump completed on 2022-07-20 23:02:30
